de330abb6673532ab560810a8f031de05ba8edbf
Dmytro Voronov <dimolg22@gmail.com>
2023-01-13 16:59:14 +0200

    add docker-compose

---
+++ src/event.entity.ts
@@ -0,0 +1,8 @@
+
+export class EventEntity {
+  id: number
+  name: string
+  description: string
+  when: Date
+  address: string
+}
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,23 +1,49 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { CreateEventDto } from "./create-event.dto";
 import { UpdateEventDto } from "./update-event.dto";
+import { EventEntity } from "./event.entity";
 
 
 @Controller('events')
 export class EventsController {
 
+  private events: EventEntity[] = []
+
   @Get()
-  findAll(){}
+  findAll(){
+    return this.events
+  }
 
   @Get(':id')
-  findOne(@Param('id') id: string){}
+  findOne(@Param('id') id: string){
+    const event = this.events.find((e)=> e.id === parseInt(id))
+    return event
+  }
+
 
   @Post()
-  create(@Body() input: CreateEventDto){}
+  create(@Body() input: CreateEventDto){
+     const event = {
+       ...input,
+       when: new Date(input.when),
+       id: this.events.length + 1
+     }
+     this.events.push(event)
+  }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() input: UpdateEventDto){}
+  update(@Param('id') id: string, @Body() input: UpdateEventDto){
+   const index = this.events.findIndex((event)=> event.id === +id)
+   this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when? new Date(input.when): this.events[index].when
+    }
+    return this.events[index]
+  }
 
   @Delete(':id')
-  remove(@Param('id') id:string){}
+  remove(@Param('id') id:string){
+     this.events.filter((event)=>event.id !== +id)
+  }
 }
